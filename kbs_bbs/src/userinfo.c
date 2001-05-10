9425381ab50322315f5b91df234b029fced20e61
bbsdev <bbsdev@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2001-05-10 16:29:06 +0000

    test
    git-svn-id: http://svn.kcn.cn/repos/kbs/branches/unlabeled-1.6.4@119 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/src/userinfo.c
+++ kbs_bbs/src/userinfo.c
@@ -20,7 +20,7 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
-
+/* KCN modified 2001.5.10 */
 #include "bbs.h"
 
 extern time_t   login_start_time;
