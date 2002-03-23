0cc010acce648cb41af2ed73fc77ca69c97b19b9
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-03-23 16:51:44 +0000

    工作到一半的未读标记管理
    git-svn-id: http://svn.kcn.cn/repos/kbs/branches/unlabeled-3.12.2@419 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/src/boards_t.c
+++ kbs_bbs/src/boards_t.c
@@ -1,25 +1,5 @@
 /*
-    Pirate Bulletin Board System
-    Copyright (C) 1990, Edward Luke, lush@Athena.EE.MsState.EDU
-    Eagles Bulletin Board System
-    Copyright (C) 1992, Raymond Rocker, rocker@rock.b11.ingr.com
-                        Guy Vega, gtvega@seabass.st.usm.edu
-                        Dominic Tynes, dbtynes@seabass.st.usm.edu
-    Copyright (C) 2002, Zhou Lin, kcn@cic.tsinghua.edu.cn
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 1, or (at your option)
-    any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+		scan complete for global variable
 */
 
 #include "bbs.h"
@@ -97,7 +77,7 @@ struct newpostdata *ptr;
             while( num > 0 ) {
                 lseek( fd, offset + num * sizeof(fh), SEEK_SET );
                 if( read( fd, filename, STRLEN ) <= 0 ||
-                        !brc_unread( filename ) )  break;
+                        !brc_unread(FILENAME2POSTTIME(  filename ) ))  break;
                 num -= step;
                 if( step < 32 )  step += step / 2;
             }
@@ -105,7 +85,7 @@ struct newpostdata *ptr;
             while( num < ptr->total ) {
                 lseek( fd, offset + num * sizeof(fh), SEEK_SET );
                 if( read( fd, filename, STRLEN ) <= 0 ||
-                        brc_unread( filename ) )  break;
+                        brc_unread(FILENAME2POSTTIME(  filename )) )  break;
                 num ++;
             }
         }
@@ -722,7 +702,7 @@ struct newpostdata *ptr;
 	if (!brc_initial(currentuser->userid,ptr->name)) {
 		ptr->unread = 1;
 	} else {
-		if (brc_unread_t(bptr->lastpost)) {
+		if (brc_unread(bptr->lastpost)) {
 			ptr->unread = 1;
 		}
 	}
